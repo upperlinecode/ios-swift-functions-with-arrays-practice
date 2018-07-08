ef54fbd924fbfd2c1668600a6f708de305d8647d
jolson615 <jolson615@gmail.com>
2018-07-08 18:11:43 -0400

    initial commit

---
+++ Functions with Arrays.playground/Contents.swift
@@ -0,0 +1,43 @@
+//: Playground - noun: a place where people can play
+
+// Level 0:
+// Create three different arrays of strings. One with your top 3 favorite movies, one with your top 4 favorite songs, and one with your top 5 favorite books!
+
+
+// Level 1:
+// Create a function that takes in an array and tells you how many items are in that array in a complete sentence.
+
+
+
+
+// Call that function on all three of the arrays you created in Level 0.
+
+
+
+// Level 2:
+// Create a function that takes in an array and prints out "I really do love ____" for every item in that array.
+
+
+
+
+// Call that function on all three of the arrays you created in level 0.
+
+
+
+// Level 3:
+// Create a function that takes in an array and tells you whether that array has more or less than 4 items in it.
+
+
+
+
+// Call that function all three of the arrays you created in level 0.
+
+
+
+// Level 4:
+// Create a function that takes in two arrays and tells whether your first array or your second array has more items in it.
+
+
+
+
+// Call the function on some different combos of the arrays you created in level 0. 
